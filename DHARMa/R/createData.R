7e2b91f774d4fa8c830775867ea8c964bb61dfe7
Florian Hartig <florianhartig@users.noreply.github.com>
2016-06-14 23:25:51 +0200

    minor updates

--- DHARMa/R/createData.R
+++ DHARMa/R/createData.R
@@ -1,7 +1,7 @@
 #' Function to simulate data
 #' @export
 #' @param overdispersion if this is a numeric value, it will be used as the sd of a random normal variate that is added to the linear predictor. Alternatively, a random function can be provided that takes as input the linear predictor. 
-#' @example /inst/examples/createPrior.R
+#' @example /inst/examples/createDataHelp.R
 createData <- function(replicates=1, sampleSize = 10, intercept = 0, fixedEffects = 1, numGroups = 10, randomEffectVariance = 1, overdispersion = 0.5, family = poisson(), scale = 1, cor = 0, roundPoissonVariance = NULL, quadraticFixedEffects = NULL){
   
   nPredictors = length(fixedEffects)
@@ -39,7 +39,7 @@ createData <- function(replicates=1, sampleSize = 10, intercept = 0, fixedEffect
     else if (family$family == "binomial") observedResponse = rbinom(n = sampleSize, 1, prob = linkResponse)
     else if (family$family == "poisson") {
       if(is.null(roundPoissonVariance)) observedResponse = rpois(n = sampleSize, lambda = linkResponse)
-      else observedResponse = round(rnorm(n = length(linkResponse), mean = linkResponse, sd = sqrt(linkResponse) * roundPoissonVariance))
+      else observedResponse = round(rnorm(n = length(linkResponse), mean = linkResponse, sd = roundPoissonVariance))
     }
     
     else stop("wrong link argument supplied")
--- DHARMa/R/plotSimulatedResiduals.R
+++ DHARMa/R/plotSimulatedResiduals.R
@@ -1,16 +1,68 @@
 #' creates Poisson data overdispersion and random intercept
-plotSimulatedResiduals <- function(simulationOutput){
+plotSimulatedResiduals <- function(simulationOutput, quantreg = F){
+  
+  
+  pred = simulationOutput$fittedPredictedResponse
+  
+  resid = simulationOutput$scaledResiduals
+  
+  dat = data.frame(pred, resid)
+  
+  
   par(mfrow = c(1,2), oma = c(0,1,2,1))
   
-  gap::qqunif(simulationOutput$scaledResiduals,pch=21,bg="blue",bty="n", logscale = F)
+  gap::qqunif(simulationOutput$scaledResiduals,pch=2,bty="n", logscale = F, col = "black", cex = 0.6, main = "QQ plot residuals", cex.main = 1)
   #hist(simulationOutput$scaledResiduals, main = "Distribution of scaled residuals", breaks = 50, freq = F)
   #lines(density(simulationOutput$scaledResiduals, na.rm = T, from = 0, to = 1, kernel = "rectangular", bw = 0.01, cut = 0.01), col = "red")
   
-  plot(simulationOutput$fittedPredictedResponse, simulationOutput$scaledResiduals, xlab = "predicted", ylab = "Residual", main = "Residual vs. predicted")
+  plot(simulationOutput$fittedPredictedResponse, simulationOutput$scaledResiduals, xlab = "Predicted", ylab = "Residual", main = "Residual vs. predicted\n lines should match", cex.main = 1)
+  
+
+  
+
+  if(quantreg == F){
+    
+    lines(smooth.spline(simulationOutput$fittedPredictedResponse, simulationOutput$scaledResiduals, df = 10), lty = 2, lwd = 2, col = "red")
+    
+    abline(h = 0.5, col = "red", lwd = 2)
+
+  }else{
+    
+    require(quantreg)
+    dat <- plyr::arrange(dat,pred)
+    fit<-quantreg::rqss(resid~qss(pred,constraint="N"),tau=0.5,data = dat)
+    lines(unique(dat$pred)[-1],fit$coef[1] + fit$coef[-1], col = "red", lwd = 2)
+    abline(h = 0.5, col = "red", lwd = 2)
+    
+    fit<-quantreg::rqss(resid~qss(pred,constraint="N"),tau=0.25,data = dat)
+    lines(unique(dat$pred)[-1],fit$coef[1] + fit$coef[-1], col = "green", lwd = 2, lty =2)
+    abline(h = 0.25, col = "green", lwd = 2, lty =2)
+    
+    fit<-quantreg::rqss(resid~qss(pred,constraint="N"),tau=0.75,data = dat)
+    lines(unique(dat$pred)[-1],fit$coef[1] + fit$coef[-1], col = "blue", lwd = 2, lty = 2)
+    abline(h = 0.75, col = "blue", lwd = 2, lty =2)   
+  }
+  
+  
+
   
   mtext("DHARMa scaled residual plots", outer = T)
 }
 
+#' Plots a generic residual plot with spline 
+plotResiduals <- function(pred, res){
+  
+  plot(pred, res)
+  
+  lines(smooth.spline(simulationOutput$fittedPredictedResponse, simulationOutput$scaledResiduals, df = 10), lty = 2, lwd = 2, col = "red")
+  
+  abline(h = 0.5, col = "red", lwd = 2)
+  
+}
+
+
+
+#plotSimulatedResiduals(simulationOutput)
 
 #plot(simulationOutput$observedResponse, simulationOutput$scaledResiduals, xlab = "predicted", ylab = "Residual", main = "Residual vs. predicted")
 
