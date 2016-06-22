7dc4a45cc4563e00193b0fe6a9f672cc339083e2
Florian Hartig <florianhartig@users.noreply.github.com>
2016-06-22 21:57:29 +0200

    deploy updates

--- DHARMa/examples/install.R
+++ DHARMa/examples/install.R
@@ -1,7 +1,7 @@
 library(devtools)
-install_url("https://dl.dropboxusercontent.com/s/hlg8j0d85ftijem/DHARMa_0.0.0.9000.tar.gz", dependencies = T)
+install_url("https://dl.dropboxusercontent.com/s/hlg8j0d85ftijem/DHARMa.tar.gz", dependencies = T)
 
-library(DHARMa)
+ library(DHARMa)
 ?DHARMa
 vignette("Demo", package="DHARMa")
 
