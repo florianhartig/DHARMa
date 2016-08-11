287c966b1610f18299050f901ca993339538354b
Florian Hartig <florianhartig@users.noreply.github.com>
2016-08-11 15:22:10 +0200

    package help / description updates

--- DHARMa/inst/examples/createDataHelp.R
+++ DHARMa/inst/examples/createDataHelp.R
@@ -1,3 +1,15 @@
-testData = createData(sampleSize = 500, intercept = 1, fixedEffects = c(1), overdispersion = 0, family = poisson(), quadraticFixedEffects = c(-3))
+testData = createData(sampleSize = 500, intercept = 2, fixedEffects = c(1), overdispersion = 0, family = poisson(), quadraticFixedEffects = c(-3), randomEffectVariance = 0)
 
+par(mfrow = c(1,2))
 plot(testData$Environment1, testData$observedResponse)
+hist(testData$observedResponse)
+
+# with zero-inflation
+
+
+testData = createData(sampleSize = 500, intercept = 2, fixedEffects = c(1), overdispersion = 0, family = poisson(), quadraticFixedEffects = c(-3), randomEffectVariance = 0, pZeroInflation = 0.6)
+
+par(mfrow = c(1,2))
+plot(testData$Environment1, testData$observedResponse)
+hist(testData$observedResponse)
+
