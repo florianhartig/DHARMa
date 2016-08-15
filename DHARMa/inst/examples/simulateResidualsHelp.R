75dbaf524fbe30d3540092c1127c1c2ea730d439
Florian Hartig <florianhartig@users.noreply.github.com>
2016-08-15 19:57:38 +0200

    help update

--- DHARMa/inst/examples/simulateResidualsHelp.R
+++ DHARMa/inst/examples/simulateResidualsHelp.R
@@ -1,5 +1,11 @@
-testData = createData(sampleSize = nData, overdispersion = 0.5, family = poisson())
+testData = createData(sampleSize = 50, overdispersion = 0.5, family = poisson())
 fittedModel <- glmer(observedResponse ~ Environment1 + (1|group), family = "poisson", data = testData)
 
 simulationOutput <- simulateResiduals(fittedModel = fittedModel)
 plotSimulatedResiduals(simulationOutput = simulationOutput)
+
+# plot directly
+simulationOutput <- simulateResiduals(fittedModel = fittedModel, plot = T)
+
+# create simulations with refitting
+simulationOutput <- simulateResiduals(fittedModel = fittedModel, n = 10, refit = T, plot = T)
