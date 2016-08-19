0fe397691643f90723e0d92aa2bbe7f95166bfe4
Florian Hartig <florianhartig@users.noreply.github.com>
2016-08-19 23:59:46 +0200

    test updates

--- DHARMa/R/testsResiduals.R
+++ DHARMa/R/testsResiduals.R
@@ -59,14 +59,15 @@ testZeroInflation <- function(simulationOutput, print = T, plot = T){
 #' @param time the time, in the same order as the data points
 #' @param print whether to print output
 #' @param plot whether to plot output
-#' @note This function is provide as a base recommendation. 
-#' @details performs the Durbin-Watson Test and plots the residuals against time
+#' @note It is possible to not specify x and y. In this case, random x and y values are created. The sense of this option is to test the rate of false positives under the current residual structure (random x/y corresponds to H0: no spatial autocorrelation). This may be useful because it may be that the test doesn't have noninal error rates due to some problem in the residual structure that is different from spatial autocorrelation
+#' @details The function performs a Durbin-Watson test on the uniformly scaled residuals, and plots the residuals against time. The DB test was originally be designed for normal residuals. In simulations, I didn't see a problem with this setting though. The alternative is to transform the uniform residuals to normal residuals and perform the DB test on those.
 #' @seealso \code{\link{testUniformDistribution}}, \code{\link{testZeroInflation}}, \code{\link{testSimulatedResiduals}}, \code{\link{testSpatialAutocorrelation}}
 #' @export
-testTemporalAutocorrelation <- function(simulationOutput, time, print = T, plot = T){
+testTemporalAutocorrelation <- function(simulationOutput, time = NULL, print = T, plot = T){
   
- 
-  out = lmtest::dwtest(simulationOutput$scaledResiduals ~ time)
+  if(is.null(time)) time = sample.int(simulationOutput$nObs, simulationOutput$nObs)
+  
+  out = lmtest::dwtest(simulationOutput$scaledResiduals ~ 1, order.by = time)
   
   if(print == T) out
   if(plot == T) {
@@ -82,21 +83,31 @@ testTemporalAutocorrelation <- function(simulationOutput, time, print = T, plot 
 #' @param y the x coordinate, in the same order as the data points
 #' @param print whether to print output
 #' @param plot whether to plot output
-#' @note This function is provide as a base recommendation. 
+#' @note It is possible to not specify x and y. In this case, random x and y values are created. The sense of this option is to test the rate of false positives under the current residual structure (random x/y corresponds to H0: no spatial autocorrelation). This may be useful because it may be that the test doesn't have noninal error rates due to some problem in the residual structure that is different from spatial autocorrelation
 #' @details performs the Durbin-Watson Test against euklidian distance and plots the residuals against time
 #' @seealso \code{\link{testUniformDistribution}}, \code{\link{testZeroInflation}}, \code{\link{testTemporalAutocorrelation}}, \code{\link{testSimulatedResiduals}}
 #' @import grDevices
 #' @export
-testSpatialAutocorrelation <- function(simulationOutput, x, y, print = T, plot = T){
+testSpatialAutocorrelation <- function(simulationOutput, x = NULL, y = NULL, print = T, plot = T){
   
-  out = lmtest::dwtest(simulationOutput$scaledResiduals ~ I(sqrt(x^2 + y^2)))
+  if(is.null(x)) x = runif(simulationOutput$nObs, -1,1)
+
+  if(is.null(y)) y = runif(simulationOutput$nObs, -1,1)
+
+  
+  distMat <- as.matrix(dist(cbind(x, y)))
+  
+  invDistMat <- 1/distMat
+  diag(invDistMat) <- 0
+  
+  out = ape::Moran.I(simulationOutput$scaledResiduals, weight = invDistMat)
   
   if(print == T) out
   if(plot == T) {
     
     col = colorRamp(c("red", "white", "blue"))(simulationOutput$scaledResiduals)
     
-    plot(x,y, col = rgb(col, max = 255) )
+    plot(x,y, col = rgb(col, maxColorValue = 255) )
   }
   return(out)
 }
