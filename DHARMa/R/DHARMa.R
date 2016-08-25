1897be44a764d195cc0699d277a734f1a13ac5ab
Florian Hartig <florianhartig@users.noreply.github.com>
2016-08-25 11:31:42 +0200

    added print / plot generics

--- DHARMa/R/DHARMa.R
+++ DHARMa/R/DHARMa.R
@@ -3,7 +3,7 @@
 #' @docType package
 #' @description The DHARMa package uses a simulation-based approach to create readily interpretable scaled residuals from fitted generalized linear mixed models. Currently supported are lme4, glm (except quasi-distributions) and lm model classes. The resulting residuals are standardized to values between 0 and 1 and can be interpreted as intuitively as residuals from a linear regression. The package also provides a number of plot and test functions for typical model mispecification problem, such as over/underdispersion, zero-inflation, and spatial and temporal autocorrelation.
 #' @details See index / vignette for details
-#' @seealso \code{\link{simulatedResiduals}}
+#' @seealso \code{\link{simulateResiduals}}
 #' @examples 
 #' vignette("Demo", package="DHARMa")
 NULL
@@ -11,20 +11,21 @@ NULL
 
 #' Plot simulated residuals
 #' 
-#' @param simulationOutput an object with simulated residuals created by \code{\link{simulateResiduals}}
-#' @param ... options of \code{\link{plotSimulatedResiduals}}
+#' @param x an object with simulated residuals created by \code{\link{simulateResiduals}}
+#' @param ... optional arguments to pass on to \code{\link{plotSimulatedResiduals}}
 #' @export
-plot.DHARMa <- function(simulationOutput, ...){
-  plotSimulatedResiduals(simulationOutput, ...)
+plot.DHARMa <- function(x, ...){
+  plotSimulatedResiduals(x, ...)
 }
 
 #' Print simulated residuals
 #' 
-#' @param simulationOutput an object with simulated residuals created by \code{\link{simulateResiduals}}
+#' @param x an object with simulated residuals created by \code{\link{simulateResiduals}}
+#' @param ... optional arguments to pass on to \code{\link{plotSimulatedResiduals}}
 #' @export
-print.DHARMa <- function(simulationOutput){
+print.DHARMa <- function(x, ...){
   print(paste("Class DHARMa with simulated residuals based on", simulationOutput$nSim, "simulations with refit =", simulationOutput$refit))
   print("see ?DHARMa::simulateResiduals for help")
   print("-----------------------------")
-  print(simulationOutput$scaledResiduals)
+  print(x$scaledResiduals)
 } 
