77c372b77f70bb0856cd5a1077e87861e024d120
Florian Hartig <florianhartig@users.noreply.github.com>
2016-08-25 11:31:28 +0200

    test updates

--- DHARMa/tests/testthat/testModelTypes.R
+++ DHARMa/tests/testthat/testModelTypes.R
@@ -2,15 +2,22 @@ context("Tests DHARMa functions on all implemented model types")
 
 library(lme4)
 
-runEverything = function(fittedModel){
+runEverything = function(fittedModel, testData){
   simulationOutput <- simulateResiduals(fittedModel = fittedModel)
+  
   plotSimulatedResiduals(simulationOutput = simulationOutput)
-  testUniformDistribution(simulationOutput = simulationOutput)
+  plotResiduals(pred = testData$Environment1, simulationOutput$scaledResiduals, quantreg = F)
+
+  testUniformity(simulationOutput = simulationOutput)
   testZeroInflation(simulationOutput = simulationOutput)
   testTemporalAutocorrelation(simulationOutput = simulationOutput, time = runif(length(simulationOutput$scaledResiduals )))
   testSpatialAutocorrelation(simulationOutput = simulationOutput, x = runif(length(simulationOutput$scaledResiduals )), y =  runif(length(simulationOutput$scaledResiduals )))
+  
+  simulationOutput2 <- simulateResiduals(fittedModel = fittedModel, refit = T, n = 25)
+  testOverdispersion(simulationOutput2)
+  testOverdispersionParametric(fittedModel)
+  
 }
-
 
 
 test_that("lm gaussian works", 
@@ -18,7 +25,7 @@ test_that("lm gaussian works",
             skip_on_cran()
             testData = createData(sampleSize = 200, overdispersion = 0, randomEffectVariance = 0, family = gaussian())
             fittedModel <- lm(observedResponse ~ Environment1 , data = testData)
-            runEverything(fittedModel)
+            runEverything(fittedModel, testData)
           }
 )
 
@@ -28,16 +35,17 @@ test_that("glm gaussian works",
             skip_on_cran()
             testData = createData(sampleSize = 200, overdispersion = 0, randomEffectVariance = 0, family = gaussian())
             fittedModel <- glm(observedResponse ~ Environment1 , data = testData)
-            runEverything(fittedModel)
+            runEverything(fittedModel, testData)
           }
 )
+
 
 test_that("lmer gaussian works", 
           {
             skip_on_cran()
             testData = createData(sampleSize = 200, overdispersion = 0, randomEffectVariance = 0, family = gaussian())
             fittedModel <- lmer(observedResponse ~ Environment1 + (1|group) , data = testData)
-            runEverything(fittedModel)
+            runEverything(fittedModel, testData)
           }
 )
 
@@ -47,7 +55,7 @@ test_that("glm binomial 1/0 works",
             skip_on_cran()
             testData = createData(sampleSize = 200, overdispersion = 0, randomEffectVariance = 0, family = binomial())
             fittedModel <- glm(observedResponse ~ Environment1 , family = "binomial", data = testData)
-            runEverything(fittedModel)
+            runEverything(fittedModel, testData)
           }
 )
 
@@ -55,9 +63,9 @@ test_that("glm binomial 1/0 works",
 test_that("glm binomial n/k works", 
           {
             skip_on_cran()
-            testData = createData(sampleSize = 200, overdispersion = 0, randomEffectVariance = 0, family = binomial(), bionomialTrials = 20)
+            testData = createData(sampleSize = 200, overdispersion = 0, randomEffectVariance = 0, family = binomial(), binomialTrials = 20)
             fittedModel <- glm(cbind(observedResponse1,observedResponse0)  ~ Environment1 , family = "binomial", data = testData)
-            runEverything(fittedModel)
+            runEverything(fittedModel, testData)
           }
 )
 
@@ -69,7 +77,7 @@ test_that("glmer binomial 1/0 works",
             
             testData = createData(sampleSize = 200, overdispersion = 2, family = binomial())
             fittedModel <- glmer(observedResponse ~ Environment1 + (1|group) , family = "binomial", data = testData)
-            runEverything(fittedModel)
+            runEverything(fittedModel, testData)
           }
 )
 
@@ -78,9 +86,9 @@ test_that("glmer binomial n/k works",
           {
             skip_on_cran()
             
-            testData = createData(sampleSize = 200, overdispersion = 2, family = binomial(), bionomialTrials = 20)
+            testData = createData(sampleSize = 200, overdispersion = 2, family = binomial(), binomialTrials = 20)
             fittedModel <- glmer(cbind(observedResponse1,observedResponse0) ~ Environment1 + (1|group) , family = "binomial", data = testData)
-            runEverything(fittedModel)
+            runEverything(fittedModel, testData)
           }
 )
 
@@ -90,7 +98,7 @@ test_that("glm poisson works",
             skip_on_cran()
             testData = createData(sampleSize = 200, overdispersion = 0.5, randomEffectVariance = 1, family = poisson(), roundPoissonVariance = 0.1, pZeroInflation = 0.1)
             fittedModel <- glm(observedResponse ~ Environment1 , family = "poisson", data = testData)
-            runEverything(fittedModel)
+            runEverything(fittedModel, testData)
           }
 )
 
@@ -100,8 +108,8 @@ test_that("glmer poisson works",
           {
             skip_on_cran()
             testData = createData(sampleSize = 200, overdispersion = 0.5, randomEffectVariance = 1, family = poisson(), roundPoissonVariance = 0.1, pZeroInflation = 0.1)
-            fittedModel <- glmer(observedResponse ~ Environment1 + (1|group) + (1|ID), family = "poisson", data = testData)
-            runEverything(fittedModel)
+            fittedModel <- glmer(observedResponse ~ Environment1 + (1|group) + (1|ID), family = "poisson", data = testData, control=glmerControl(optCtrl=list(maxfun=20000) ))
+            runEverything(fittedModel, testData)
           }
 )
 
