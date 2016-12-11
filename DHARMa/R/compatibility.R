6d9c2b2eab0824fbf4d2f65851e3588a65c09a48
Florian Hartig <florianhartig@users.noreply.github.com>
2016-12-11 12:27:23 +0100

    bugfix / variable name compatibility

--- DHARMa/R/compatibility.R
+++ DHARMa/R/compatibility.R
@@ -3,11 +3,11 @@
 #' 
 #' This function overwrites the standard fitted function for GAM
 #' @note See explanation at 
-#' @param x fitted model
+#' @param object fitted model
 #' @export
-fitted.gam <- function(x){
-  class(x) = "glm"
-  out = stats::fitted(x)
+fitted.gam <- function(object, ...){
+  class(object) = "glm"
+  out = stats::fitted(object, ...)
   names(out) = as.character(1:length(out))
   out
 }
