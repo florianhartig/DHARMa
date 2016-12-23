2de9e672dc46b1b5a1e33bc5e5c77cdd868a4dc5
Florian Hartig <florianhartig@users.noreply.github.com>
2016-12-23 14:07:16 +0100

    Update CRAN.R

--- DHARMa/util/CRAN.R
+++ DHARMa/util/CRAN.R
@@ -1,15 +1,26 @@
 
+## Prepare release
+
+* All issues resolved?
+* Appropriate unit tests created?
+* Update help / description files -> date, version number, etc.
+* Make sure RStudio check function checks out
+* Cross-check compatibility with CRAN requirements
+
+## Test release
+
+* Make sure that help files / tests don't take too long (5s on slow computer max for CRAN)
+* Check TravisCI automatic tests
+* Check package in http://win-builder.r-project.org/upload.aspx
+* Check Reverse dependencies
+  * https://github.com/yihui/crandalf
+  * devtools::revdep()
 
 
-# http://win-builder.r-project.org/upload.aspx
 
 
 
-# Reverse dependencies
 
-# https://github.com/yihui/crandalf
-
-devtools::revdep()
 
 revdep_maintainers
 
