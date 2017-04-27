da79bf290e2ce1c19ae8230dfabcd5eab0c1c97d
Florian Hartig <florian@remote239-086.home.uni-freiburg.de>
2017-04-27 21:23:46 +0200

    slight syntax change in tests

--- DHARMa/R/testsResiduals.R
+++ DHARMa/R/testsResiduals.R
@@ -136,9 +136,9 @@ testZeroInflation <- function(simulationOutput,  plot = T, alternative = "more")
 #' @details The function performs a Durbin-Watson test on the uniformly scaled residuals, and plots the residuals against time. The DB test was originally be designed for normal residuals. In simulations, I didn't see a problem with this setting though. The alternative is to transform the uniform residuals to normal residuals and perform the DB test on those.
 #' @seealso \code{\link{testUniformity}}, \code{\link{testZeroInflation}}, \code{\link{testSimulatedResiduals}}, \code{\link{testSpatialAutocorrelation}}, \code{\link{testOverdispersion}}, \code{\link{testOverdispersionParametric}}
 #' @export
-testTemporalAutocorrelation <- function(simulationOutput, time , plot = T){
+testTemporalAutocorrelation <- function(simulationOutput, time = NULL , plot = T){
   
-  if(length(time) == 1) if(time == "random") time = sample.int(simulationOutput$nObs, simulationOutput$nObs)
+  if(is.null(time)) time = sample.int(simulationOutput$nObs, simulationOutput$nObs)
   
   out = lmtest::dwtest(simulationOutput$scaledResiduals ~ 1, order.by = time)
   
@@ -154,21 +154,29 @@ testTemporalAutocorrelation <- function(simulationOutput, time , plot = T){
 #' This function performs a standard test for spatial autocorrelation on the simulated residuals
 #' 
 #' @param simulationOutput an object with simulated residuals created by \code{\link{simulateResiduals}}
-#' @param x the x coordinate, in the same order as the data points. If set to "random", random values will be created
-#' @param y the x coordinate, in the same order as the data points. If set to "random", random values will be created
+#' @param x the x coordinate, in the same order as the data points. If not provided, random values will be created
+#' @param y the x coordinate, in the same order as the data points. If not provided, random values will be created
+#' @param distMat optional distance matrix. If not provided, a distance matrix will be calculated based on x and y. See details for explanation
 #' @param plot whether to plot output
-#' @note It is possible to not specify x and y. In this case, random x and y values are created. The sense of this option is to test the rate of false positives under the current residual structure (random x/y corresponds to H0: no spatial autocorrelation). This may be useful because it may be that the test doesn't have noninal error rates due to some problem in the residual structure that is different from spatial autocorrelation
-#' @details performs Moran.I from the package ape to test against euklidian distance and plots the residuals against space
+#' @details The function performs Moran.I test from the package ape, based on the provided distance matrix of the data points. 
+#' 
+#' There are several ways to specify this distance. If a distance matrix (distMat) is provided, calculations will be based on this distance matrix, and x,y coordinates will only used for the plotting (if provided)
+#' If distMat is not provided, the function will calculate the euclidian distances between x,y coordinates, and test Moran.I based on these distances.
+#' 
+#'    It is possible to not specify x and y. In this case, random x and y values are created. The sense of this option is to test the rate of false positives under the current residual structure (random x/y corresponds to H0: no spatial autocorrelation). This may be useful because it may be that the test doesn't have noninal error rates due to some problem in the residual structure that is different from spatial autocorrelation 
+#' 
 #' @seealso \code{\link{testUniformity}}, \code{\link{testZeroInflation}}, \code{\link{testTemporalAutocorrelation}}, \code{\link{testSimulatedResiduals}}, \code{\link{testOverdispersion}}, \code{\link{testOverdispersionParametric}}
 #' @import grDevices
 #' @export
-testSpatialAutocorrelation <- function(simulationOutput, x , y , plot = T){
+testSpatialAutocorrelation <- function(simulationOutput, x = NULL, y  = NULL, distMat = NULL, plot = T){
   
-  if(length(x) == 1) if (x == "random") x = runif(simulationOutput$nObs, -1,1) 
-  if(length(y) == 1) if (y == "random") y = runif(simulationOutput$nObs, -1,1)
-
+  if( !is.null(x) & !is.null(distMat) ) 
+  # if not provided, fill x and y with random numbers (Null model)
+  if(is.null(x)) x = runif(simulationOutput$nObs, -1,1) 
+  if(is.null(y)) y = runif(simulationOutput$nObs, -1,1)
   
-  distMat <- as.matrix(dist(cbind(x, y)))
+  # if not provided, create distance matrix based on x and y
+  if(is.null(distMat)) distMat <- as.matrix(dist(cbind(x, y)))
   
   invDistMat <- 1/distMat
   diag(invDistMat) <- 0
