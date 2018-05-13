75dc0c4a749dab8ddf4d7a3ed769567c061fc187
Florian Hartig <florian.hartig@biologie.uni-regensburg.de>
2018-05-13 18:44:54 +0200

    Startup messages

---
+++ DHARMa/R/zzz.R
@@ -0,0 +1,17 @@
+getMessage <- function(){ 
+  library(help=DHARMa)$info[[1]] -> version
+  version <- version[pmatch("Version",version)]
+  um <- strsplit(version," ")[[1]]
+  version <- um[nchar(um)>0][2]
+  hello <- paste("This is DHARMa ",version,". For overview type 'help(\"DHARMa-package\")'.\n\nNote that, since v0.1.6.3, DHARMa includes support for glmmTMB, but there are still a few limitations with this package. In particular, due to the way glmmTMB simulates, there can be spuriuous correlations when plotting residuals against fitted. Please consult https://github.com/florianhartig/DHARMa/issues/16 for details.",sep="")
+  return(hello)
+}
+
+.onLoad <- function(...) {
+  packageStartupMessage("Note that, since v0.1.6.3, DHARMa includes support for glmmTMB, but there are still a few limitations with this package. In particular, due to the way glmmTMB simulates, there can be spuriuous correlations when plotting residuals against fitted. Please consult https://github.com/florianhartig/DHARMa/issues/16 for details.")
+}
+
+.onAttach <- function(...) { 
+  packageStartupMessage("Note that, since v0.1.6.3, DHARMa includes support for glmmTMB, but there are still a few limitations with this package. In particular, due to the way glmmTMB simulates, there can be spuriuous correlations when plotting residuals against fitted. Please consult https://github.com/florianhartig/DHARMa/issues/16 for details.")
+}
+
