feac53c11b484248752c2888c34e8b0f6e40a232
Florian Hartig <florian.hartig@biologie.uni-regensburg.de>
2018-08-20 19:39:54 +0200

    help update for createDharma

--- DHARMa/inst/examples/createDharmaHelp.R
+++ DHARMa/inst/examples/createDharmaHelp.R
@@ -1,3 +1,29 @@
+## READING IN HAND-CODED SIMULATIONS
+
+testData = createData(sampleSize = 50, randomEffectVariance = 0)
+fittedModel <- glm(observedResponse ~ Environment1, data = testData, family = "poisson")
+
+# in DHARMA, using the simulate.glm function of glm 
+sims = simulateResiduals(fittedModel)
+plot(sims, quantreg = FALSE)
+
+# Doing the same with a handcode simulate function. 
+# of course this code will only work with a 1-par glm model
+simulateMyfit <- function(n=10, fittedModel){
+  int = coef(fittedModel)[1]
+  slo = coef(fittedModel)[2]
+  pred = exp(int + slo * testData$Environment1)
+  predSim = replicate(n, rpois(length(pred), pred))
+  return(predSim)
+}
+
+sims = simulateMyfit(250, fittedModel)
+
+dharmaRes <- createDHARMa(simulatedResponse = sims, observedResponse = testData$observedResponse, fittedPredictedResponse = predict(fittedModel, type = "response"), integer = T)
+plot(dharmaRes, quantreg = FALSE)
+
+## A BAYESIAN EXAMPLE
+
 \dontrun{
   
   # This example shows how to check the residuals for a 
