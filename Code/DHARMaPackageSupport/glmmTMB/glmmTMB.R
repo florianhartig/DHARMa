d92bdfac31134cd2c02bc3bd84aa84d2ff04fe9b
florianhartig <florian@sg-p0-040111.uni-regensburg.de>
2019-02-12 14:05:04 +0700

    updates

--- Code/DHARMaPackageSupport/glmmTMB/glmmTMB.R
+++ Code/DHARMaPackageSupport/glmmTMB/glmmTMB.R
@@ -11,11 +11,15 @@ summary(m1)
 simulate(m1)
 class(m1)
 
+res <- simulateResiduals(m1)
+plot(res)
+
+
 ### creating new data based on the fitted models. Residuals should now be perfect
 
-Salamanders$counts2 = simulate(m2)
+Salamanders$counts2 = simulate(m1)$sim_1
 
-(m2 <- glmmTMB(count2~spp + mined + (1|site), 
+(m2 <- glmmTMB(counts2~spp + mined + (1|site), 
                zi=~spp + mined, 
                family=nbinom2, Salamanders))
 
@@ -52,7 +56,7 @@ plot(res)
 
 ### creating new data based on the fitted models. Residuals should now be perfect
 
-Salamanders$counts2 = simulate(m)
+Salamanders$count2 = simulate(m)$sim_1
 
 m <- glmmTMB(count2~ mined + (1|site), 
              zi=~mined, 
@@ -76,7 +80,7 @@ plot(res)
 
 # creating a new response based on the fitted response 
 
-Salamanders$counts2 = simulate(m2)
+Salamanders$count2 = simulate(m2)$sim_1
 
 (m2 <- glmmTMB(count2~spp + mined + (1|site), 
                zi=~spp + mined, 
@@ -93,6 +97,45 @@ plot(res)
                family=truncated_poisson, Salamanders))
 res = simulateResiduals(m3)
 plot(res)
+
+
+## Binomial model
+data(cbpp, package="lme4")
+(m4 <- glmmTMB(cbind(incidence, size-incidence) ~ period + (1 | herd),
+                  data=cbpp, family=binomial))
+
+res = simulateResiduals(m4)
+plot(res)
+
+
+
+## Dispersion model
+
+sim1=function(nfac=40, nt=100, facsd=.1, tsd=.15, mu=0, residsd=1)
+{
+  dat=expand.grid(fac=factor(letters[1:nfac]), t= 1:nt)
+  n=nrow(dat)
+  dat$REfac=rnorm(nfac, sd= facsd)[dat$fac]
+  dat$REt=rnorm(nt, sd= tsd)[dat$t]
+  dat$x=rnorm(n, mean=mu, sd=residsd) + dat$REfac + dat$REt
+  return(dat)
+}
+set.seed(101)
+d1 = sim1(mu=100, residsd =10)
+d2 = sim1(mu=200, residsd =5)
+d1$sd="ten"
+d2$sd="five"
+dat = rbind(d1, d2)
+
+
+m5 = glmmTMB(x~sd+(1|t), dispformula=~sd, dat)
+
+res = simulateResiduals(m5)
+plot(res)
+
+
+fixef(m5)$disp
+c(log(5^2), log(10^2)-log(5^2)) #expected dispersion model coefficients
 
 
 
