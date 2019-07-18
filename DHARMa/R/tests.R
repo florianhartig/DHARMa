2dea87526bee4250f57d37351d1174e258c4e75d
florianhartig <florian@wlanhome187107.uni-regensburg.de>
2019-07-18 12:34:35 +0200

    warning about missing coordinates / time in tests
    fixes #108

--- DHARMa/R/tests.R
+++ DHARMa/R/tests.R
@@ -281,7 +281,10 @@ testTemporalAutocorrelation <- function(simulationOutput, time = NULL , alternat
   
   alternative <- match.arg(alternative)
   
-  if(is.null(time)) time = sample.int(simulationOutput$nObs, simulationOutput$nObs)
+  if(is.null(time)){
+    time = sample.int(simulationOutput$nObs, simulationOutput$nObs)
+    warning("DHARMa::testTemporalAutocorrelation - no time argument provided, using random times for each data point")
+  } 
   
   out = lmtest::dwtest(simulationOutput$scaledResiduals ~ 1, order.by = time, alternative = alternative)
   
@@ -321,10 +324,17 @@ testSpatialAutocorrelation <- function(simulationOutput, x = NULL, y  = NULL, di
   
   alternative <- match.arg(alternative)
 
-  if( !is.null(x) & !is.null(distMat) ) warning("coordinates and distMat provided, coordinates will only be used for plotting")
+  if( (!is.null(x) | !is.null(y)) & !is.null(distMat) ) warning("both coordinates and distMat provided, calculations will be done based on the distance matrix, coordinates will only be used for plotting")
   # if not provided, fill x and y with random numbers (Null model)
-  if(is.null(x)) x = runif(simulationOutput$nObs, -1,1) 
-  if(is.null(y)) y = runif(simulationOutput$nObs, -1,1)
+  if(is.null(x)){
+    x = runif(simulationOutput$nObs, -1,1) 
+    warning("DHARMa::testSpatialAutocorrelation - no x coordinates provided, using random values for each data point")
+  } 
+  
+  if(is.null(y)){
+    y = runif(simulationOutput$nObs, -1,1)
+    warning("DHARMa::testSpatialAutocorrelation - no x coordinates provided, using random values for each data point")
+  } 
   
   # if not provided, create distance matrix based on x and y
   if(is.null(distMat)) distMat <- as.matrix(dist(cbind(x, y)))
