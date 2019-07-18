d7968a918ad6665e917bc14ece5b3a14cc2ad0b9
florianhartig <florian@wlanhome187107.uni-regensburg.de>
2019-07-18 13:04:08 +0200

    overdispersion tests

--- Code/DHARMaPerformance/Overdispersion.R
+++ Code/DHARMaPerformance/Overdispersion.R
@@ -3,12 +3,22 @@ library(DHARMa)
 library(lme4)
 library(performance)
 
-testData = createData(sampleSize = 1000, overdispersion = 1)
+testData = createData(sampleSize = 1000, overdispersion = 0.5)
 
 fittedModel <- glmer(observedResponse ~ Environment1 + (1|group), 
                     data = testData, family = poisson)
+summary(fittedModel)
 
-simulationOutput <- simulateResiduals(fittedModel = fittedModel)
+fittedModel <- glmer(observedResponse ~ Environment1 + (1|group) + (1|ID), 
+                     data = testData, family = poisson)
+summary(fittedModel)
+
+
+simulationOutput <- simulateResiduals(fittedModel = fittedModel, n = 2000)
 
 testDispersion(simulationOutput)
 performance::check_overdispersion(fittedModel)
+
+
+simulationOutput <- simulateResiduals(fittedModel = fittedModel, n = 2000, refit = T)
+testDispersion(simulationOutput)
