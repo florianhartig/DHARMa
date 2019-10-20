1e8a10e846741ef65df3c30e6e211d1431894414
florianhartig <florian@UR-MPB-2018-FH.fritz.box>
2019-10-20 18:47:43 +0200

    transformQuantiles function

--- DHARMa/R/transformQuantiles.R
+++ DHARMa/R/transformQuantiles.R
@@ -6,7 +6,7 @@
 #' @param quantileFunction the quantile function of the desired distribution
 #' @param outlierValue the value that should be assigned to residuals that are 0/1 and thus typically mapped to -Inf / Inf for continous distributions with infinte support
 #' 
-#' @details See also https://github.com/florianhartig/DHARMa/issues/39
+#' @details Some of the papers on simulated quantile residuals transforming the residuals (which are natively uniform) back to a normal distribution. I presume this is because of the larger familiarity of most users with normal residuals. Personally, I never considered this desirable, for the reasons explained in https://github.com/florianhartig/DHARMa/issues/39, but with this function, I wanted to give users the option to plot normal residuals if they so wish. 
 #' 
 #' @example inst/examples/transformQuantilesHelp.R
 #'
@@ -17,9 +17,3 @@ transformQuantiles <- function(res, quantileFunction = qnorm, outlierValue = 7){
   res = ifelse(res == Inf, outlierValue, res)
   return(res)
 }
-
-
-
-
-
-
