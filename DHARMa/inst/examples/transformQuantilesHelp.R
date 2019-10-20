1e7e9f98cf60b9e29cc47cfdc5b4860fd82be584
florianhartig <florian@UR-MPB-2018-FH.fritz.box>
2019-10-20 20:03:35 +0200

    bugfixes

--- DHARMa/inst/examples/transformQuantilesHelp.R
+++ DHARMa/inst/examples/transformQuantilesHelp.R
@@ -1,1 +1,8 @@
+
+set.seed(1)
+testData = createData(sampleSize = 200, family = poisson())
+fittedModel <- glmer(observedResponse ~ Environment1 + (1|group), 
+                     family = "poisson", data = testData)
+res <- simulateResiduals(fittedModel = fittedModel)
 plot(res$fittedPredictedResponse, transformQuantiles(res))
+
