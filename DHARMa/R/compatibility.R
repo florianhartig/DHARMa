1cbc28d4b70d6d579b5b3c1b86ed7888b708f5e8
florianhartig <florianhartig@users.noreply.github.com>
2019-12-20 09:06:59 +0100

    removed testModel from DHARMa

--- DHARMa/R/compatibility.R
+++ DHARMa/R/compatibility.R
@@ -1,35 +1,4 @@
 
-
-
-#' Test DHARMa compatibility
-#' 
-#' This helper function tests the compatibility of a model with DHARMa by trying to run various functions that are needed
-#' 
-#' @importFrom lme4 fixef
-#' @importFrom lme4 ranef
-#' @importFrom spaMM response
-#' @importFrom spaMM update_resp
-#' 
-#' @param fittedModel the fitted model
-#' 
-#' @author Florian Hartig
-#' @export
-#' 
-testModel <-function(fittedModel){
-  
-  try(family(fittedModel))
-  try(class(fittedModel)[1])
-  try(nobs(fittedModel))
-  try(getResponse(fittedModel))
-  try(simulate(fittedModel, nsim = 10))
-  try(predict(fittedModel))
-  try(coef(fittedModel))
-  try(ranef(fittedModel))
-  try(fixef(fittedModel))
-  try(refit(fittedModel, newresp = getResponse(fittedModel)))
-  
-}
-
 # New S3 methods
 
 #' Get model response
