5c7a543225fd624bbda238b053c5f0fdcac168b0
florianhartig <florianhartig@users.noreply.github.com>
2020-03-27 20:28:04 +0100

    help update

--- DHARMa/inst/examples/testSpatialAutocorrelationHelp.R
+++ DHARMa/inst/examples/testSpatialAutocorrelationHelp.R
@@ -21,10 +21,13 @@ res2 = recalculateResiduals(res, group = testData$group)
 testSpatialAutocorrelation(res)
 
 
-# carefull with clustered data and conditional / unconditional simulations
-# this originates from https://github.com/florianhartig/DHARMa/issues/81
+# carefull when using REs to account for spatially clustered (but not grouped)
+# data. this originates from https://github.com/florianhartig/DHARMa/issues/81
 
-# Assume our data is divided into clusters, and we use a RE to take out cluster effects
+
+# Assume our data is divided into clusters, where observations are close together
+# but not at te same point, and we suspect that observations in clusters are
+# autocorrelated
 
 clusters = 100
 subsamples = 10
@@ -33,6 +36,9 @@ size = clusters * subsamples
 testData = createData(sampleSize = size, family = gaussian(), numGroups = clusters )
 testData$x  = rnorm(clusters)[testData$group] + rnorm(size, sd = 0.01)
 testData$y  = rnorm(clusters)[testData$group] + rnorm(size, sd = 0.01)
+
+# It's a good idea to use a RE to take out the cluster effects. This accounts
+# for the autocorrelation within clusters 
 
 library(lme4)
 fittedModel <- lmer(observedResponse ~ Environment1 + (1|group), data = testData)
@@ -51,12 +57,10 @@ testSpatialAutocorrelation(res2,
                            x =  aggregate(testData$x, list(testData$group), mean)$x, 
                            y = aggregate(testData$y, list(testData$group), mean)$x)
 
-# For lme4, possible to simulated residuals conditional on fitted REs (re.form)
-# This takes out most of the RSA - a remainder is probably due the shrinkage
-# of the REs
+# For lme4, it's also possible to simulated residuals conditional on fitted 
+# REs (re.form). Conditional on the fitted REs (i.e. accounting for the clusters)
+# the residuals should now be indepdendent. The remaining RSA we see here is 
+# probably due to the RE shrinkage
 
 res = simulateResiduals(fittedModel, re.form = NULL)
 testSpatialAutocorrelation(res, x =  testData$x, y = testData$y)
-
-
-
