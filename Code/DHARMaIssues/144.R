fda8cc36043f46cc5564df47320c7196a78d93c2
florianhartig <florianhartig@users.noreply.github.com>
2020-04-19 22:16:44 +0200

    small updates

--- Code/DHARMaIssues/144.R
+++ Code/DHARMaIssues/144.R
@@ -1,5 +1,5 @@
 library(mgcv)
-library(mgcViz)
+#library(mgcViz)
 library(DHARMa)
 
 
--- DHARMa/R/startup.R
+++ DHARMa/R/startup.R
@@ -1,7 +1,7 @@
 print.DHARMa.startupInfo <- function()
 { 
   version <- packageVersion('DHARMa')
-  hello <- paste("This is DHARMa ",version,". For overview type '?DHARMa'. For recent changes, type news(package = 'DHARMa')", "Note: Syntax of plotResiduals has changed in 0.3.0, see ?plotResiduals for details" ,sep="")
+  hello <- paste("This is DHARMa ",version,". For overview type '?DHARMa'. For recent changes, type news(package = 'DHARMa')", " Note: Syntax of plotResiduals has changed in 0.3.0, see ?plotResiduals for details" ,sep="")
   packageStartupMessage(hello)
 }
 
