306de61a1d7ddc31a9e5498a581e73530af8e97f
florianhartig <florianhartig@users.noreply.github.com>
2020-06-22 10:45:58 +0200

    ISEC

---
+++ Code/DHARMaTeaching/ISEC2020/ISEC-2020-live.R
@@ -0,0 +1,6 @@
+# ISEC DHARMa skills showcase - script for live editing
+# During the skills showcase, please use Slack to interact, otherwise twitter @florianhartig
+
+library(DHARMa)
+library(lme4)
+library(glmmTMB)
