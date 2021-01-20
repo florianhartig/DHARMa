def0c73d38c6c93de71ff17761c81c27e656754a
florianhartig <florianhartig@users.noreply.github.com>
2021-01-20 11:25:05 +0100

    update run benchmarks

--- DHARMa/inst/examples/runBenchmarksHelp.R
+++ DHARMa/inst/examples/runBenchmarksHelp.R
@@ -1,35 +1,22 @@
-returnP <- function(counter=NULL){
-  testData = createData(sampleSize = 20, family = gaussian())
-  fittedModel <- lm(observedResponse ~ Environment1, data = testData)
-  res <- simulateResiduals(fittedModel = fittedModel, n = 250)
-  out <- c(testUniformity(res)$p.value, res$scaledResiduals)
-  return(out)
-}
-
-# testing a single return
-returnP()
-
-# running benchmark
-out = runBenchmarks(returnP, nRep = 20)
-
-# running benchmark parallel
-out = runBenchmarks(returnP, nRep = 500, parallel = T)
-
-
-returnP <- function(control = 0){
-  testData = createData(sampleSize = 20, family = poisson(), overdispersion = control)
+returnStatistics <- function(control = 0){
+  testData = createData(sampleSize = 20, family = poisson(), overdispersion = control, randomEffectVariance = 0)
   fittedModel <- glm(observedResponse ~ Environment1, data = testData, family = poisson())
   res <- simulateResiduals(fittedModel = fittedModel, n = 250)
-  out <- suppressWarnings(c(testUniformity(res)$p.value, res$scaledResiduals))
+  out <- c(testUniformity(res, plot = F)$p.value, testDispersion(res, plot = F)$p.value)
   return(out)
 }
 
 # testing a single return
-returnP(control = 1)
+returnStatistics()
 
 # running benchmark
-out = runBenchmarks(returnP, controlValues = c(0,0.5,1), nRep = 20)
+out = runBenchmarks(returnStatistics, nRep = 20)
 
 # running benchmark parallel
-out = runBenchmarks(returnP, controlValues = c(0,0.5,1), nRep = 500, parallel = T)
+out = runBenchmarks(returnStatistics, nRep = 500, parallel = T)
+
+# include control values
+out = runBenchmarks(returnStatistics, controlValues = c(0,0.5,1), nRep = 20)
+
+
 
