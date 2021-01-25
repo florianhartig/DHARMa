1d278952e3a115e5bdf0d7f127870ff5a6b05459
florianhartig <florianhartig@users.noreply.github.com>
2021-01-25 22:17:07 +0100

    test file updates

--- Code/DHARMaPackageSupport/GLMMadaptive/Untitled.R
+++ Code/DHARMaPackageSupport/GLMMadaptive/Untitled.R
@@ -1,33 +1,13 @@
 devtools::install_github("drizopoulos/GLMMadaptive")
 library(GLMMadaptive)
+library(DHARMa)
 
 
 # simulate some data
 set.seed(123L)
-n <- 500
-K <- 15
-t.max <- 25
 
-betas <- c(-2.13, -0.25, 0.24, -0.05)
-D <- matrix(0, 2, 2)
-D[1:2, 1:2] <- c(0.48, -0.08, -0.08, 0.18)
-
-times <- c(replicate(n, c(0, sort(runif(K-1, 0, t.max)))))
-group <- sample(rep(0:1, each = n/2))
-DF <- data.frame(year = times, group = factor(rep(group, each = K)))
-X <- model.matrix(~ group * year, data = DF)
-Z <- model.matrix(~ year, data = DF)
-
-b <- cbind(rnorm(n, sd = sqrt(D[1, 1])), rnorm(n, sd = sqrt(D[2, 2])))
-id <- rep(1:n, each = K)
-eta.y <- as.vector(X %*% betas + rowSums(Z * b[id, ]))
-DF$y <- rbinom(n * K, 1, plogis(eta.y))
-DF$id <- factor(id)
-
-################################################
-
-fm1 <- mixed_model(fixed = y ~ year * group, random = ~ 1 | id, data = DF,
-                   family = binomial())
+DF = testData = createData(sampleSize = 500, overdispersion = 0, randomEffectVariance = 0.000, family = poisson())
+fm1 <-  fittedModel <- mixed_model(fixed = observedResponse ~ Environment1, random = ~ 1 | group, data = DF, family = poisson())
 
 res <- simulateResiduals(fm1)
 
@@ -57,6 +37,11 @@ family(fm1)
 model.frame(fm1)
 simulate(fm1)
 
+# Check update behavior
+
+DF2 = testData = createData(sampleSize = 500, overdispersion = 0, randomEffectVariance = 0.000, family = poisson())
+fm2 = update(fm1, data = DF2)
+fm3 <-  fittedModel <- mixed_model(fixed = observedResponse ~ Environment1, random = ~ 1 | group, data = DF2, family = poisson())
 
 
 
