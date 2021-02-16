4ecb4e99196c493a494a2be6d918a9df8a0ab994
florianhartig <florianhartig@users.noreply.github.com>
2021-02-16 09:36:14 +0100

    updates 201 tests

--- Code/DHARMaIssues/201.R
+++ Code/DHARMaIssues/201.R
@@ -55,6 +55,73 @@ spread <- function(x) sd((x - res$fittedPredictedResponse) / expectedSD)
 testGeneric(res, summary = spread)
 
 
+##### Tests #####
+
+
+returnStatistics <- function(control = 0){
+  testData = createData(sampleSize = 1000, family = poisson(), overdispersion = control, numGroups = 100)
+  fittedModel <- lme4::glmer(observedResponse ~ Environment1 + (1|group), data = testData, family = poisson())
+  
+  out = list()
+  
+  res <- simulateResiduals(fittedModel = fittedModel, n = 250)
+  out$DHARMaDefault = testDispersion(res, plot = FALSE)$p.value
+  
+  # using Var instead of SD
+  expectedVar = sd(res$simulatedResponse)^2
+  spread2 <- function(x) var(x - res$fittedPredictedResponse) /  expectedVar
+  out$DHARMaDefaultVar = testGeneric(res, spread2, plot = F)$p.value
+  
+  res2 <- simulateResiduals(fittedModel = fittedModel, n = 250, re.form = ~0)
+  out$DHARMaDefaultConditional = testDispersion(res2, plot = FALSE)$p.value
+  
+  out$DHARMaPearson = testDispersion(res, plot = FALSE, type = "Pearson")$p.value
+  
+  # NEW Option 1: dispersion measured by variance of residuals against uniform 
+  obs1 = var(res$scaledResiduals) * 12
+  sim1 = replicate(1000, {var(runif(length(res$scaledResiduals)))}) * 12
+  out$VarResSimuUni = DHARMa:::getP(simulated = sim1, observed = obs1, plot = F, alternative = "two.sided")
+  
+  # NEW Option 2: dispersion measured by variance of the normal, which should be something like a simulation-based deviance residual? 
+  # issue is how to handle the outliers
+  obs2 = var(residuals(res, quantileFunction = qnorm, outlierValues = c(-7,7))) 
+  sim2 = replicate(1000, {var(rnorm(length(res$scaledResiduals)))}) # I know we could do this analytical, but I'm lazy
+  out$VarResSimuNorm = DHARMa:::getP(simulated = sim2, observed = obs2, plot = F, alternative = "two.sided")
+  
+  # NEW Option 3 - probably problematic for situations with binomial / possion only 0 / 1 simulations
+  res <- simulateResiduals(fittedModel = fittedModel, re.form  = NULL)
+  expectedSD = apply(res$simulatedResponse, 1, sd)
+  spread <- function(x) sd((x - res$fittedPredictedResponse) / max(expectedSD, 0.001)) 
+  out$PearsonSimu = testGeneric(res, summary = spread, plot = F)$p.value
+
+  return(out)
+}
+
+# testing a single return
+returnStatistics()
+
+# running benchmark
+out = runBenchmarks(returnStatistics, nRep = 100)
+
+names = c("DHARMa standard", "DHARMa var", "DHARMa conditional", "PearsonParametric",  "VarResSimuUni", "VarResSimuNorm", "PearsonSimu")
+
+par(mfrow = c(3,3))
+for(i in 1:7) {
+  hist(out$simulations[,i], freq= F, breaks =20, main = names[i])
+  abline(h = 1, col = "red")
+}
+
+control = seq(0,1.5,len = 20)
+out = runBenchmarks(returnStatistics, nRep = 100, controlValues = control)
+
+
+par(mfrow = c(1,1))
+
+res = out$summaries$propSignificant
+matplot(res$controlValues, res[,-1], type = "l")
+# legend("bottomright", colnames(res[,-1]), col = 1:4, lty = 1:4)
+
+legend("bottomright", names, col = 1:7, lty = 1:7)
 
 
 
