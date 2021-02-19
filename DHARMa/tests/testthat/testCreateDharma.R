302a0eccb69c0c16c2be8662918fe02782f9ec10
florianhartig <florianhartig@users.noreply.github.com>
2021-02-19 12:02:52 +0100

    moved recalculate to dharma tests

--- DHARMa/tests/testthat/testCreateDharma.R
+++ DHARMa/tests/testthat/testCreateDharma.R
@@ -1,9 +1,9 @@
 
-context("createDHARMa")
+context("DHARMa object creations and re-calculations")
 
 
 
-test_that("Standard creation works", {
+test_that("createDHARMa works", {
   
   testData = createData(sampleSize = 200, family = poisson(), 
                         randomEffectVariance = 0, numGroups = 5)
@@ -33,4 +33,46 @@ test_that("Standard creation works", {
 
   
 })
+
+
+
+test_that("RecalcuateResiduals works", {
+  
+  # set.seed(123)
+  
+  testData = createData(sampleSize = 200, family = poisson(), numGroups = 20, 
+                        randomEffectVariance = 1)
+  
+  #len = sum(testData$group == 1)
+  #testData$group[testData$group == 1] = sample(c(1,21),20, replace = T)
+  
+  fittedModel <- glm(observedResponse ~ Environment1, family = "poisson", data = testData)
+  
+  simulationOutput = simulateResiduals(fittedModel)
+  plot(simulationOutput, quantreg = F)
+  dim(simulationOutput$simulatedResponse)
+  
+  ##### only grouping #####
+  
+  simulationOutput2 = recalculateResiduals(simulationOutput, group = testData$group)
+  plot(simulationOutput2, quantreg = F)
+  
+  ##### only selection ##### 
+  
+  # simulationOutput2 = recalculateResiduals(simulationOutput, sel = 1) # doesn't work, doesn't have to work!
+  
+  testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = 1:2))) == 2)
+  testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = c(1,6,8)))) == 3)
+  testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = testData$group == 1))) == 10)
+  
+  ##### selection and grouping #####
+  
+  # only i data point and group, doesn't work
+  simulationOutput2 = recalculateResiduals(simulationOutput, group = testData$group, sel = 1)
+  
+})
+
+
+
+
 
