890fb6ca30c0c6a289babdc5b8aa765e4fd50c41
EttnerAndreas <70816016+EttnerAndreas@users.noreply.github.com>
2021-03-02 14:29:17 +0100

    adding test for testthat several checks passed, looks like function is still working, several checks for new selection function 'sel'. Any test implemented did pass the test.
    I would recommend merging and closing Issue #246

--- DHARMa/tests/testthat/testDharmaClass.R
+++ DHARMa/tests/testthat/testDharmaClass.R
@@ -58,18 +58,49 @@ test_that("RecalcuateResiduals works", {
   ##### only selection ##### 
   
   # simulationOutput2 = recalculateResiduals(simulationOutput, sel = 1) # doesn't work, doesn't have to work!
-  
+  simulationOutput2 = recalculateResiduals(simulationOutput, sel = 1:400) 
   testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = 1:2))) == 2)
   testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = c(1,6,8)))) == 3)
-  testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = testData$group == 1))) == 10)
+  testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = testData$group == 1))) == 10) 
   
   ##### selection and grouping #####
+  # selection by different variables
+  simulationOutput2 = recalculateResiduals(simulationOutput, sel = testData$time) 
+  plot(simulationOutput2, quantreg = F)
   
-  #simulationOutput2 = recalculateResiduals(simulationOutput, group = testData$group, sel = 1)   # only i data point and group, doesn't work
+  
+  #selecting = float   -   wrong dimensions (Sel has length 0)
+  # testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = 1.0 :20.0))) == 20) 
+  # simulationOutput2 = recalculateResiduals(simulationOutput, group = testData$group, sel = 1.0 : 10.0)  # not working, don´t has to work  
+  
+  
+  # testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = 1.5 :10.7))) == 10) # not checking float 
+  # simulationOutput2 = recalculateResiduals(simulationOutput, group = testData$group, sel = 1.5:10.6)    # not working, don´t has to work 
+  
+  
+  # selecting = negative Values
+  # testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = -10 : 5))) == 194) # not working, don´t has to work 
+  testthat::expect_true( length( residuals(recalculateResiduals(simulationOutput, sel = -10 : -5))) == 194)
+  simulationOutput3 = recalculateResiduals(simulationOutput, sel =  -10 : -3)
+  
+  
+  # checking NULL functions
+  simulationOutput3 = recalculateResiduals(simulationOutput, sel =  NULL)
+  # simulationOutput3 = recalculateResiduals(simulationOutput, seed = NULL) not working, don´t has to work
+  simulationOutput3 = recalculateResiduals(simulationOutput, seed = 0)
+  simulationOutput3 = recalculateResiduals(simulationOutput, group = NULL, sel = NULL)
+  simulationOutput3 = recalculateResiduals(simulationOutput, group = NULL, sel = c(1:5,8,9), method = "PIT")
+  simulationOutput3 = recalculateResiduals(simulationOutput, group = NULL, sel = c(1:5,8,9), method = "traditional")
+  
+  
+  # checking for 
+  simulationOutput4 = recalculateResiduals(simulationOutput, group = 1:simulationOutput$nObs, sel = simulationOutput$nObs/2:simulationOutput$nObs, method = "traditional")
+  plot(simulationOutput4)
+  simulationOutput4 = recalculateResiduals(simulationOutput, group = 1:simulationOutput$nObs, sel = simulationOutput$nObs/3.5:simulationOutput$nObs, method = "PIT")
+  plot(simulationOutput4)
+  
   
 })
-
-
 
 
 
