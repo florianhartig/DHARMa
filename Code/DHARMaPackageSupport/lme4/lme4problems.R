07706a80c3eead2f60b72003538722fd443d5500
florianhartig <florianhartig@users.noreply.github.com>
2021-03-18 09:30:20 +0100

    Update lme4problems.R

--- Code/DHARMaPackageSupport/lme4/lme4problems.R
+++ Code/DHARMaPackageSupport/lme4/lme4problems.R
@@ -67,16 +67,26 @@ library(lme4)
 library(statmod)
 
 #create toy dataset
-df1 <- data.frame(production=c(15,12,10,9,6,8,9,5,3,3,2,1,0,0,0,0), Treatment_Num=c(1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4), Genotype=c(1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2))
+df1 <- data.frame(production=c(15,12,10,9,6,8,9,5,3,3,2,1,0,0,0,0), 
+                  Treatment_Num=c(1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4), 
+                  Genotype=c(1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2))
 
 #run tweedie model
 df1_tweedie <- glmer(production ~ Treatment_Num +(1|Genotype),
                      data = df1, family=tweedie(var.power=1.9,link.power=0))
 
-#load DHARMa
-require(DHARMa)
-#I get an error when I try running this line:
-df1_tweedie_res <- simulateResiduals(df1_tweedie)
+
+simulate()
+
+
+#run tweedie model
+library(glmmTMB)
+tweedieGLMMTMB <- glmmTMB(production ~ Treatment_Num +(1|Genotype),
+                     data = df1, family=glmmTMB::tweedie())
+
+simulate(tweedieGLMMTMB)
+
+res <- simulateResiduals(tweedieGLMMTMB, plot = T)
 
 
 
