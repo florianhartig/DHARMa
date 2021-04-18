123537c079f05878e66bd23b78b9875504f77175
EttnerAndreas <70816016+EttnerAndreas@users.noreply.github.com>
2021-04-18 14:37:44 +0200

    adding examples at DHARMa/inst/examples rename/restructure example

---
+++ DHARMa/inst/examples/hurricanes.R
@@ -0,0 +1,22 @@
+# Loading hurricanes dataset
+
+library(DHARMa)
+data(hurricanes)
+
+
+library(glmmTMB)
+originalModelGAM = glmmTMB(alldeaths ~ scale(MasFem) *
+                             (scale(Minpressure_Updated.2014) + scale(NDAM)),
+                           data = hurricanes, family = nbinom2)
+
+# Residual checks with DHARMa
+res <- simulateResiduals(originalModelGAM)
+plot(res)
+
+# no significant deviation in the general plot, but try this
+# which was highlighted by https://www.theguardian.com/science/grrlscientist/2014/jun/04/hurricane-gender-name-bias-sexism-statistics
+plotResiduals(res, hurricanes$NDAM)
+
+# we also find temporal autocorrelation
+res2 = recalculateResiduals(res, group = hurricanes$Year)
+testTemporalAutocorrelation(res2, time = unique(hurricanes$Year))
