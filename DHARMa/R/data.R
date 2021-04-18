06e63661a4283d6b5aae2ca82235ed03b62f67ec
EttnerAndreas <70816016+EttnerAndreas@users.noreply.github.com>
2021-04-19 00:12:21 +0200

    change dataset remove empty spaces to avoid errors

--- DHARMa/R/data.R
+++ DHARMa/R/data.R
@@ -12,12 +12,12 @@
 #' @param Name. Name the hurricanes was named after (name)
 #' @param MasFem. Rating of the Name, if it is more masculin or feminine (1:11). In the range 1 = very masculine, 11 = very feminine
 #' @param MinPressure_before. Minimum air-pressure (909-1002)
-#' @param `Minpressure_Updated 2014`. Minimum airpressure (909-1003)
+#' @param Minpressure_Updated_2014. Minimum airpressure (909-1003)
 #' @param Gender_MF. Gender rating binary (male/female). Male = 0, female = 1)
 #' @param Category. strength of the hurricane in categories (1:7). (1 = not at all, 7 = very intense)
 #' @param alldeaths. Deaths counted (1:256)
 #' @param NDAM. normalized damage in millions (1:75.000). The raw (dollar) amounts of property damage caused by hurricanes were obtained, and the unadjusted dollar amounts were normalized to 2013 monetary values by adjusting them to inflation, wealth and population density
-#' @param `Elapsed Yrs`. elapsed since the occurrence of hurricanes (1:63)
+#' @param Elapsed_Yrs. elapsed since the occurrence of hurricanes (1:63)
 #' @param Source. MWR/wikipedia ()
 #' @param ZMasFem. scaled (MasFem)
 #' @param ZMinPressure_A. scaled (Minpressure_Updated 2014)
