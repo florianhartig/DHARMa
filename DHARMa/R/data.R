a954d49a53d42f8299c30694daece8c7e77b46da
EttnerAndreas <70816016+EttnerAndreas@users.noreply.github.com>
2021-04-19 23:20:02 +0200

    final fix, re-writing help-file/data.R file

--- DHARMa/R/data.R
+++ DHARMa/R/data.R
@@ -1,33 +1,34 @@
 #' hurricanes
 #'
 #' This dataset contains hurricanes in the USA between 1950 and 2012 their strength, names ..., see parameter for more informations.
+#'
 #' @name hurricanes
 #' @title hurricanes dataset residual analysis
 #' @aliases hurricanes
 #' @docType data
 #' @keywords hurricanes, DHARMa, simulateResiduals,
 #'
-#'
-#' @param Year. Year the hurricanes appeared (1950-2012)
-#' @param Name. Name the hurricanes was named after (name)
-#' @param MasFem. Rating of the Name, if it is more masculin or feminine (1:11). In the range 1 = very masculine, 11 = very feminine
-#' @param MinPressure_before. Minimum air-pressure (909-1002)
-#' @param Minpressure_Updated_2014. Minimum airpressure (909-1003)
-#' @param Gender_MF. Gender rating binary (male/female). Male = 0, female = 1)
-#' @param Category. strength of the hurricane in categories (1:7). (1 = not at all, 7 = very intense)
-#' @param alldeaths. Deaths counted (1:256)
-#' @param NDAM. normalized damage in millions (1:75.000). The raw (dollar) amounts of property damage caused by hurricanes were obtained, and the unadjusted dollar amounts were normalized to 2013 monetary values by adjusting them to inflation, wealth and population density
-#' @param Elapsed_Yrs. elapsed since the occurrence of hurricanes (1:63)
-#' @param Source. MWR/wikipedia ()
-#' @param ZMasFem. scaled (MasFem)
-#' @param ZMinPressure_A. scaled (Minpressure_Updated 2014)
-#' @param ZNDAM. scaled (NDAM)
-#' @return a dataframe
-#'
 #' @source https://theoreticalecology.wordpress.com/2021/04/17/hurricanes-and-himmicanes-revisited-with-dharma/
 #' @references Jung et al., PNAS, 2014
-#' @usage{data(hurricanes)}
 #' @seealso \code{\link{simulateResiduals}}
-#' @format A data frame with 93 rows and 14 variables
+#'
+#' @format A 'data.frame': 93 obs. of  14 variables
+#' \describe{
+#'   \item{Year}{Year the hurricanes appeared (1950-2012)}
+#'   \item{Name}{Name the hurricanes was named after (name)}
+#'   \item{MasFem}{Rating of the Name, if it is more masculin or feminine (1:11). In the range 1 = very masculine, 11 = very feminine}
+#'   \item{MinPressure_before}{Minimum air-pressure (909-1002)}
+#'   \item{Minpressure_Updated_2014}{Minimum air-pressure (909-1003)}
+#'   \item{Gender_MF}{eGender rating binary (male/female). Male = 0, female = 1)}
+#'   \item{Category}{strength of the hurricane in categories (1:7). (1 = not at all, 7 = very intense)}
+#'   \item{alldeaths}{Deaths counted (1:256)}
+#'   \item{NDAM}{normalized damage in millions (1:75.000). The raw (dollar) amounts of property damage caused by hurricanes were obtained, and the unadjusted dollar amounts were normalized to 2013 monetary values by adjusting them to inflation, wealth and population density}
+#'   \item{Elapsed_Yrs}{elapsed since the occurrence of hurricanes (1:63)}
+#'   \item{Source}{MWR/wikipedia ()}
+#'   \item{ZMasFem}{scaled (MasFem)}
+#'   \item{ZMinPressure_A}{scaled (Minpressure_Updated_2014)}
+#'   \item{ZNDAM}{scaled (NDAM)}
+#'   ...
+#' }
 #' @example inst/examples/hurricanes.R
 NULL
