0cf63019107a5a2799a8e35c5bc54ce5b80ca87a
EttnerAndreas <70816016+EttnerAndreas@users.noreply.github.com>
2021-04-23 09:28:49 +0200

    small change

--- Code/DHARMaData/DataCreationMaster.R
+++ Code/DHARMaData/DataCreationMaster.R
@@ -1,6 +1,6 @@
 # Data creation
   
-hurricanes <- read_csv("../Code/DHARMaData/raw-data/hurricane.csv")
+hurricanes <- readr::read_csv("../Code/DHARMaData/raw-data/hurricane.csv")
 
 colnames(hurricanes) = c("Year","Name","MasFem","MinPressure_before",  "Minpressure_Updated_2014","Gender_MF","Category","alldeaths", "NDAM","Elapsed_Yrs","Source","ZMasFem",  "ZMinPressure_A","ZNDAM")
 str(hurricanes)
