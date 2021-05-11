83057236b276c3b0372e1c16098a1fd4ff5ed53f
Florian Hartig <florianhartig@users.noreply.github.com>
2021-05-11 10:28:31 +0200

    started to address problems

--- DHARMa/R/compatibility.R
+++ DHARMa/R/compatibility.R
@@ -28,7 +28,7 @@
 
 # getResiduals
 
-######### Generics #############
+######### Generics / New S3 methods #############
 
 #' get possible models
 #' 
@@ -37,7 +37,8 @@
 #' @keywords internal
 getPossibleModels<-function()c("lm", "glm", "negbin", "lmerMod", "lmerModLmerTest", "glmerMod", "gam", "bam", "glmmTMB", "HLfit", "MixMod")
 
-# New S3 methods
+weightsWarning = "Model was fit with prior weights. These will be ignored in the simulation. See ?getSimulations for details"
+
 
 #' Get model response
 #'
@@ -57,33 +58,6 @@ getPossibleModels<-function()c("lm", "glm", "negbin", "lmerMod", "lmerModLmerTes
 getObservedResponse <- function (object, ...) {
   UseMethod("getObservedResponse", object)
 }
-
-#' @rdname getObservedResponse
-#' @export
-getObservedResponse.default <- function (object, ...){
-  out = model.frame(object)[,1]
-
-  # check for weights in k/n case
-  if(family(object)$family %in% c("binomial", "betabinomial") & "(weights)" %in% colnames(model.frame(object))){
-    x = model.frame(object)
-    out = out * x$`(weights)`
-  }
-
-  # check for k/n binomial
-  if(is.matrix(out)){
-    if(!(ncol(out) == 2)) securityAssertion("nKcase - wrong dimensions of response")
-    if(!(family(object)$family %in% c("binomial", "betabinomial"))) securityAssertion("nKcase - wrong family")
-
-    out = out[,1]
-  }
-
-  # observation is factor - unlike lme4 and older, glmmTMB simulates nevertheless as numeric
-  if(is.factor(out)) out = as.numeric(out) - 1
-
-  return(out)
-}
-
-weightsWarning = "Model was fit with prior weights. These will be ignored in the simulation. See ?getSimulations for details"
 
 #' Get model simulations
 #'
@@ -112,69 +86,19 @@ getSimulations <- function (object, nsim = 1 , type = c("normal", "refit"), ...)
   UseMethod("getSimulations", object)
 }
 
-#' @rdname getSimulations
-#' @export
-getSimulations.default <- function (object, nsim = 1, type = c("normal", "refit"), ...){
-
-  type <- match.arg(type)
-
-  out = simulate(object, nsim = nsim , ...)
-
-  if (type == "normal"){
-    if(family(object)$family %in% c("binomial", "betabinomial")){
-      if("(weights)" %in% colnames(model.frame(object))){
-        x = model.frame(object)
-        out = out * x$`(weights)`
-      } else if (is.matrix(out[[1]])){
-        # this is for the k/n binomial case
-        out = as.matrix(out)[,seq(1, (2*nsim), by = 2)]
-      } else if(is.factor(out[[1]])){
-        if(nlevels(out[[1]]) != 2){
-          warning("The fitted model has a factorial response with number of levels not equal to 2 - there is currently no sensible application in DHARMa that would lead to this situation. Likely, you are trying something that doesn't work.")
-        }
-        else{
-          out = data.matrix(out) - 1
-        }
-      }
-    }
-
-    if(!is.matrix(out)) out = data.matrix(out)
-  } else{
-    if(family(object)$family %in% c("binomial", "betabinomial")){
-      if (!is.matrix(out[[1]]) & !is.numeric(out)) data.frame(data.matrix(out)-1)
-    }
-  }
-
-  return(out)
-}
-
-
 #' Extract fixed effects of a supported model
 #'
 #' A wrapper to extract fixed effects of a supported model
 #'
-#' @param fittedModel a fitted model
+#' @param object a fitted model
+#' @param ... additional parameters
 #'
 #' @example inst/examples/wrappersHelp.R
 #'
-#' @importFrom lme4 fixef
-#'
 #' @seealso \code{\link{getObservedResponse}}, \code{\link{getSimulations}}, \code{\link{getRefit}}, \code{\link{getFitted}}
 #' @export
-getFixedEffects <- function(fittedModel){
-
-  if(class(fittedModel)[1] %in% c("glm", "lm", "gam", "bam", "negbin") ){
-    out  = coef(fittedModel)
-  } else if(class(fittedModel)[1] %in% c("glmerMod", "lmerMod", "HLfit")){
-    out = fixef(fittedModel)
-  } else if(class(fittedModel)[1] %in% c("glmmTMB")){
-    out = glmmTMB::fixef(fittedModel)
-    out = out$cond
-  } else {
-    out = coef(fittedModel)
-    if(is.null(out)) out = fixef(fittedModel)
-  }
-  return(out)
+getFixedEffects <- function(object,...){
+  UseMethod("getFixedEffects", object)
 }
 
 #' Get model refit
@@ -194,15 +118,6 @@ getFixedEffects <- function(fittedModel){
 #' @export
 getRefit <- function (object, newresp, ...) {
   UseMethod("getRefit", object)
-}
-
-#' @rdname getRefit
-#'
-#' @importFrom lme4 refit
-#'
-#' @export
-getRefit.default <- function (object, newresp, ...){
-  refit(object, newresp, ...)
 }
 
 #' Get fitted / predicted values
@@ -226,14 +141,6 @@ getFitted <- function (object, ...) {
   UseMethod("getFitted", object)
 }
 
-# NOTE - a bit unclear if fitted or predict should be used
-
-#' @rdname getFitted
-#' @export
-getFitted.default <- function (object,...){
-  predict(object, type = "response", re.form = ~0)
-}
-
 
 #' Get model residuals
 #'
@@ -254,13 +161,6 @@ getResiduals <- function (object, ...) {
   UseMethod("getResiduals", object)
 }
 
-# NOTE - a bit unclear if fitted or predict should be used
-
-#' @rdname getFitted
-#' @export
-getResiduals.default <- function (object,...){
-  residuals(object, type = "response")
-}
 
 #' has NA
 #'
@@ -277,6 +177,112 @@ getResiduals.default <- function (object,...){
 #   if(length(x) < as.numeric(x[length(x) ])) return(TRUE)
 #   else return(FALSE)
 # }
+
+######### DEFAULTS #############
+
+#' @rdname getObservedResponse
+#' @export
+getObservedResponse.default <- function (object, ...){
+  out = model.frame(object)[,1]
+  
+  # check for weights in k/n case
+  if(family(object)$family %in% c("binomial", "betabinomial") & "(weights)" %in% colnames(model.frame(object))){
+    x = model.frame(object)
+    out = out * x$`(weights)`
+  }
+  
+  # check for k/n binomial
+  if(is.matrix(out)){
+    if(!(ncol(out) == 2)) securityAssertion("nKcase - wrong dimensions of response")
+    if(!(family(object)$family %in% c("binomial", "betabinomial"))) securityAssertion("nKcase - wrong family")
+    
+    out = out[,1]
+  }
+  
+  # observation is factor - unlike lme4 and older, glmmTMB simulates nevertheless as numeric
+  if(is.factor(out)) out = as.numeric(out) - 1
+  
+  return(out)
+}
+
+#' @rdname getSimulations
+#' @export
+getSimulations.default <- function (object, nsim = 1, type = c("normal", "refit"), ...){
+  
+  type <- match.arg(type)
+  
+  out = simulate(object, nsim = nsim , ...)
+  
+  if (type == "normal"){
+    if(family(object)$family %in% c("binomial", "betabinomial")){
+      if("(weights)" %in% colnames(model.frame(object))){
+        x = model.frame(object)
+        out = out * x$`(weights)`
+      } else if (is.matrix(out[[1]])){
+        # this is for the k/n binomial case
+        out = as.matrix(out)[,seq(1, (2*nsim), by = 2)]
+      } else if(is.factor(out[[1]])){
+        if(nlevels(out[[1]]) != 2){
+          warning("The fitted model has a factorial response with number of levels not equal to 2 - there is currently no sensible application in DHARMa that would lead to this situation. Likely, you are trying something that doesn't work.")
+        }
+        else{
+          out = data.matrix(out) - 1
+        }
+      }
+    }
+    
+    if(!is.matrix(out)) out = data.matrix(out)
+  } else{
+    if(family(object)$family %in% c("binomial", "betabinomial")){
+      if (!is.matrix(out[[1]]) & !is.numeric(out)) data.frame(data.matrix(out)-1)
+    }
+  }
+  
+  return(out)
+}
+
+#' @rdname getFixedEffects
+#' @export
+getFixedEffects.default <- function(object, ...){
+  
+  if(class(object)[1] %in% c("glm", "lm", "gam", "bam", "negbin") ){
+    out  = coef(object)
+  } else if(class(object)[1] %in% c("glmerMod", "lmerMod", "HLfit")){
+    out = fixef(object)
+  } else if(class(object)[1] %in% c("glmmTMB")){
+    out = glmmTMB::fixef(object)
+    out = out$cond
+  } else {
+    out = coef(object)
+    if(is.null(out)) out = fixef(object)
+  }
+  return(out)
+}
+
+#' @rdname getRefit
+#'
+#' @importFrom lme4 refit
+#'
+#' @export
+getRefit.default <- function (object, newresp, ...){
+  refit(object, newresp, ...)
+}
+
+# NOTE - a bit unclear if fitted or predict should be used
+
+#' @rdname getFitted
+#' @export
+getFitted.default <- function (object,...){
+  predict(object, type = "response", re.form = ~0)
+}
+
+# NOTE - a bit unclear if fitted or predict should be used
+
+#' @rdname getFitted
+#' @export
+getResiduals.default <- function (object,...){
+  residuals(object, type = "response")
+}
 
 ######### LM #############
 
@@ -320,16 +326,57 @@ getSimulations.negbin<- function (object, nsim = 1, type = c("normal", "refit"),
 
 ######## MGCV ############
 
-# This function overwrites the standard fitted function for GAM
-
-#' @rdname getFitted
-#' @export
-getFitted.gam <- function(object, ...){
+# This function overwrites the standard fitted function for GAM, see https://github.com/florianhartig/DHARMa/issues/12
+fitted.gam <- function(object, ...){
   class(object) = "glm"
   out = stats::fitted(object, ...)
   names(out) = as.character(1:length(out))
-  out
+  return(out)
 }
+
+#' @rdname getSimulations
+#' @export
+getSimulations.gam <- function (object, nsim = 1, type = c("normal", "refit"), ...){
+  
+  type <- match.arg(type)
+  
+  if(system.file(package = "mgcViz") == ""){
+    message("mgcViz not found. When using DHARMa with mgcv, it is recommended to also install mgcViz. See Vigette, package mgcv for details")
+    out = getSimulations.default(object, nsim, type)
+  }
+  else {
+    
+    out = mgcvViz::simulate(object, nsim = nsim , ...)
+    
+    if (type == "normal"){
+      if(family(object)$family %in% c("binomial", "betabinomial")){
+        if("(weights)" %in% colnames(model.frame(object))){
+          x = model.frame(object)
+          out = out * x$`(weights)`
+        } else if (is.matrix(out[[1]])){
+          # this is for the k/n binomial case
+          out = as.matrix(out)[,seq(1, (2*nsim), by = 2)]
+        } else if(is.factor(out[[1]])){
+          if(nlevels(out[[1]]) != 2){
+            warning("The fitted model has a factorial response with number of levels not equal to 2 - there is currently no sensible application in DHARMa that would lead to this situation. Likely, you are trying something that doesn't work.")
+          }
+          else{
+            out = data.matrix(out) - 1
+          }
+        }
+      }
+      
+      if(!is.matrix(out)) out = data.matrix(out)
+    } else{
+      if(family(object)$family %in% c("binomial", "betabinomial")){
+        if (!is.matrix(out[[1]]) & !is.numeric(out)) data.frame(data.matrix(out)-1)
+      }
+    }
+  }
+  
+  return(out)
+}
+
 
 # Check that this works
 # plot(fitted(fittedModelGAM), predict(fittedModelGAM, type = "response"))
@@ -503,8 +550,8 @@ getSimulations.MixMod <- function(object, nsim = 1, type = c("normal", "refit"),
 
 #' @rdname getFixedEffects
 #' @export
-getFixedEffects.MixMod <- function(fittedModel){
-  out <- fixef(fittedModel, sub_model = "main")   
+getFixedEffects.MixMod <- function(object, ...){
+  out <- fixef(object, sub_model = "main")   
   return(out)
 }
 
