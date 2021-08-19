be76fa3d6879befeefa47c8c9d9b7a34409b4946
Florian Hartig <florianhartig@users.noreply.github.com>
2021-08-19 13:32:16 +0200

    Create 303.R

---
+++ Code/DHARMaIssues/303.R
@@ -0,0 +1,27 @@
+# set up the model structure, but don't fit the model yet
+
+library(glmmTMB)
+
+testData = createData(sampleSize = 200, overdispersion = 0, family = binomial())
+
+#  weights = weight
+fit = glmmTMB(observedResponse ~ Environment1 + (1|group) + (1|ID), family=binomial(), data=testData, doFit=FALSE)
+
+# set the variance of the random intercept term for stratum to a large, fixed value
+
+fit$parameters$theta[1] = log(1e3)
+
+# adjust the number of parameters
+
+nvarparam <- length(fit$parameters$theta)
+fit$mapArg <- list(theta=factor(c(NA, 1:(nvarparam-1))))
+
+# fit the model and summarize the model results
+
+fit2 = glmmTMB:::fitTMB(fit)
+summary(fit2)
+
+res <- simulateResiduals(fit2) # error
+
+# reasons seems to be the predict function, which doesn't allow re.form = ~0 in this case
+predict(fit2, re.form = ~0)
