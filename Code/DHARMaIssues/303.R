c7a1c799abada5719a2998ff5a45714efa5e897d
Florian Hartig <florianhartig@users.noreply.github.com>
2021-08-24 08:31:44 +0200

    Update 303.R

--- Code/DHARMaIssues/303.R
+++ Code/DHARMaIssues/303.R
@@ -2,6 +2,7 @@
 
 library(glmmTMB)
 
+set.seed(123)
 testData = createData(sampleSize = 200, overdispersion = 0, family = binomial())
 
 #  weights = weight
@@ -49,4 +50,24 @@ plot(res)
 fit2C <- glmmTMB(observedResponse ~ Environment1 + (1|group) + (1|ID), family=binomial(), data=testData)
 res<- simulateResiduals(fit2C)
 plot(res)
+
+# Workaround 1  - fit / check fixed effect model
+# Logic: when the variance is large, the only difference between the fixed and random model is the way the model reports 
+# the estimates, i.e. the mixed model has other contrasts for the grouping factor and returns a mean, while the fixed 
+# model returns some contrasts of the grouping factors. Approximately, it should be OK to check residuals with the 
+# fixed effect version of the model, and still fit the model with the RE version. 
+
+fit2D <- glmmTMB(observedResponse ~ Environment1 + group + (1|ID), family=binomial(), data=testData)
+res<- simulateResiduals(fit2D)
+plot(res)
+
+# Workaround 2 - create predictions by hand
+
+preds = predict(fit2B, re.form = NULL, type = "response")  # creates conditional predictions
+simulator = function() rbinom(length(preds), size = 1, prob = preds)
+sims = replicate(200, simulator())
+res <- createDHARMa(sims, testData$observedResponse, fittedPredictedResponse = predict(fit2B, re.form = ~0, type = "response"))
+plot(res)
+
+
 
