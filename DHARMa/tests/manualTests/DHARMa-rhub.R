099096fa7763acc2a5108c84846893e23d5d1c3d
Florian Hartig <florianhartig@users.noreply.github.com>
2022-01-12 14:17:24 +0100

    Update DHARMa-rhub.R

--- DHARMa/tests/manualTests/DHARMa-rhub.R
+++ DHARMa/tests/manualTests/DHARMa-rhub.R
@@ -1,4 +1,21 @@
+
+devtools::check_win_release()
+
 library(rhub)
 # validate_email(email = "florian.hartig@ur.de")
-rhub::check(path = "./DHARMa", platform = c(1,10,20))
-1
+
+rhub::platforms()
+pltfms = c("ubuntu-rchk")
+
+rhub::check(path = "./DHARMa", platform = pltfms)
+
+
+# submitting to 
+
+packageFile = "DHARMa_0.4.5.tar.gz"
+
+devtools::check_win_devel(packageFile)
+devtools::check_win_release(packageFile)
+devtools::check_win_oldrelease(packageFile)
+
+
