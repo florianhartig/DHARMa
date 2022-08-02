af5dbd7ba270430707c4d6a0ffb6c1a6071e5b9a
Florian Hartig <florianhartig@users.noreply.github.com>
2022-08-02 13:09:11 +0200

    updates

--- DHARMa/inst/examples/simulateLRTHelp.R
+++ DHARMa/inst/examples/simulateLRTHelp.R
@@ -10,5 +10,11 @@ m1 = glmer(observedResponse ~ Environment1 + (1|group), data = dat, family = "po
 
 m0 = glm(observedResponse ~ Environment1 , data = dat, family = "poisson")
 
-out = simulateLRT(m0, m1)
-out
+\dontrun{
+out = simulateLRT(m0, m1, n = 10)
+
+# LRT produced warnings, can inspect what's going on
+out = simulateLRT(m0, m1, saveModels = T, suppressWarnings = T, n = 10)
+summary(out$saveModels[[2]]$refittedM1) # RE SD = 0
+# Could try changing the optimizer to reduce warnings
+}
