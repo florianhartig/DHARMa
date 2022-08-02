e74c73688a3c4ffe23590635cbd5d07a940261af
danielrettelbach <daniel.rettelbach@icloud.com>
2022-08-02 12:06:00 +0200

    Update runBenchmarks.R

--- DHARMa/R/runBenchmarks.R
+++ DHARMa/R/runBenchmarks.R
@@ -59,8 +59,8 @@ runBenchmarks <- function(calculateStatistics, controlValues = NULL, nRep = 10, 
     # doesn't see to work properly
     loadedPackages = (.packages())
     parExectuer = function(x = NULL, control = NULL) calculateStatistics(control)
-    parallel::clusterExport(cl = cl, c("parExectuer", "loadedPackages"), envir = environment())
-    parallel::clusterEvalQ(cl, {for(p in loadedPackages) library(p, character.only=TRUE)})
+    parallel::clusterExport(cl = cl, varlist = ls(envir = .GlobalEnv))
+    parallel::clusterExport(cl = cl, c("loadedPackages"), envir = environment()) 
     
     # parallel::clusterExport(cl = cl, varlist = ls(envir = .GlobalEnv))
     
