93ad8cea97e286d6a9669325857d5969572082a4
Florian Hartig <florianhartig@users.noreply.github.com>
2022-09-09 17:33:48 +0200

    Update simulateLRTHelp.R

--- DHARMa/inst/examples/simulateLRTHelp.R
+++ DHARMa/inst/examples/simulateLRTHelp.R
@@ -2,19 +2,21 @@
 library(DHARMa)
 library(lme4)
 
+# create test data
 set.seed(123)
 dat <- createData(sampleSize = 200, randomEffectVariance = 1)
 
+# define Null and alternative model (should be nested)
 m1 = glmer(observedResponse ~ Environment1 + (1|group), data = dat, family = "poisson")
-
-
 m0 = glm(observedResponse ~ Environment1 , data = dat, family = "poisson")
 
 \dontrun{
+# run LRT - n should be increased to at least 250 for a real study
 out = simulateLRT(m0, m1, n = 10)
 
-# LRT produced warnings, can inspect what's going on
-out = simulateLRT(m0, m1, saveModels = T, suppressWarnings = T, n = 10)
-summary(out$saveModels[[2]]$refittedM1) # RE SD = 0
-# Could try changing the optimizer to reduce warnings
+# To inspect warnings thrown during the refits:
+out = simulateLRT(m0, m1, saveModels = T, suppressWarnings = F, n = 10)
+summary(out$saveModels[[2]]$refittedM1) # RE SD = 0, no problem
+# If there are warnings that seem problematic, 
+# could try changing the optimizer or iterations
 }
