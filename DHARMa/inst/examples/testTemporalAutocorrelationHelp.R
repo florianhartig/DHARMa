b0c5bc7d9692c3a9e4f961430abf2f6f77e4b926
Florian Hartig <florianhartig@users.noreply.github.com>
2023-01-11 13:09:53 +0100

    updated rotation help
    related to #364

--- DHARMa/inst/examples/testTemporalAutocorrelationHelp.R
+++ DHARMa/inst/examples/testTemporalAutocorrelationHelp.R
@@ -34,37 +34,81 @@ testTemporalAutocorrelation(res, time = unique(testData$time))
 
 # example to demonstrate problems with strong temporal correlations and
 # how to possibly remove them by rotating residuals
+# note that if your model allows to condition on estimated REs, this may 
+# be preferable!
 
 \dontrun{
 
-set.seed(1)
-C <- exp(-as.matrix(dist(seq(0,50,by=.5))))
-obs <- as.numeric(mvtnorm::rmvnorm(1,sigma=C))
+  set.seed(123)
+  
+  # Gaussian error 
+  
+  # Create AR data with 5 observations per time point
+  n <- 100                                              
+  x <- MASS::mvrnorm(mu = rep(0,n),
+                     Sigma = .9 ^ as.matrix(dist(1:n)) )   
+  y <- rep(x, each = 5) + 0.2 * rnorm(5*n)                       
+  times <- factor(rep(1:n, each = 5), levels=1:n)
+  levels(times)
+  group <- factor(rep(1,n*5))
+  dat0 <- data.frame(y,times,group)
+  
+  # fit model
+  model = glmmTMB(y ~ ar1(times + 0 | group), data=dat0)
+  
+  # Standard residuals show spurious problems because of autocorrelation
+  res <- simulateResiduals(model)
+  plot(res)
 
-opar <- par(mfrow = c(1,2))
-image(C, main = "Specified autocorrelation (covariance)")
-plot(obs, type = "l", main = "Time series")
-par(opar)
+  # grouped according to times, unrotated shows autocorrelation
+  res2 <- recalculateResiduals(res, group=dat0$times) 
+  testTemporalAutocorrelation(res2, time = 1:length(res2$scaledResiduals))
+  
+  # extract estimated AR1 covariance
+  cov <- VarCorr(model)
+  cov <- cov$cond$group # extract covariance matrix of REs
 
-# calculate standard DHARMa residuals
+  # grouped according to times, rotated with estimated Cov - how all fine!
+  res3 <- recalculateResiduals(res, group=dat0$times, rotation=cov) 
+  plot(res3)
+  testTemporalAutocorrelation(res3, time = 1:length(res2$scaledResiduals))
 
-## simulations from the model:
-x = replicate(1000, as.numeric(mvtnorm::rmvnorm(1,sigma=C)))
+  # Poisson error 
+  # note that for GLMMs, covariances will be estimated at the scale of the 
+  # linear predictor, while residual covariance will be at the responses scale
+  # and thus further distorted by the link. Thus, for GLMMs with a nonlinear
+  # link, there will be no exact rotation for a given covariance structure
+  
+  set.seed(123)
+  
+  # Create AR data with 5 observations per time point
+  n <- 100                                              
+  x <- MASS::mvrnorm(mu = rep(0,n),
+                     Sigma = .9 ^ as.matrix(dist(1:n)) )   
+  y <- rpois(n = n*5, lambda = exp(rep(x, each = 5)))                     
+  times <- factor(rep(1:n, each = 5), levels=1:n)
+  levels(times)
+  group <- factor(rep(1,n*5))
+  dat0 <- data.frame(y,times,group)
+  
+  # fit model
+  model = glmmTMB(y ~ ar1(times + 0 | group), data=dat0, family = poisson)
+  
+  res <- simulateResiduals(model)
 
-res <- createDHARMa(x, obs, integerResponse = F)
-plot(res)
-testTemporalAutocorrelation(res, time = 1:length(res$scaledResiduals))
+  # grouped according to times, unrotated
+  res2 <- recalculateResiduals(res, group=dat0$times) 
+  testTemporalAutocorrelation(res2, time = 1:length(res2$scaledResiduals))
+  
+  # grouped according to times, rotated with estimated Cov - problems remain
+  cov <- VarCorr(model)
+  cov <- cov$cond$group # extract covariance matrix of REs
+  res3 <- recalculateResiduals(res, group=dat0$times, rotation=cov) 
+  testTemporalAutocorrelation(res3, time = 1:length(res2$scaledResiduals))
 
-# calculated rotated DHARMa residuals to remove temporal correlation
-# this only works if the autocorrelation is homogeneous / stationary
-res <- createDHARMa(x, obs, integerResponse = F, rotation = C)
-testUniformity(res)
-testTemporalAutocorrelation(res, time = 1:length(res$scaledResiduals))
-
-# the same, but with a covariance based on simulations
-res <- createDHARMa(x, obs, integerResponse = F, rotation = "estimated")
-testUniformity(res)
-testTemporalAutocorrelation(res, time = 1:length(res$scaledResiduals))
-
+  # grouped according to times, rotated with covariance estimated from residual 
+  # simulations at the response scale
+  res4 <- recalculateResiduals(res, group=dat0$times, rotation="estimated") 
+  testTemporalAutocorrelation(res4, time = 1:length(res2$scaledResiduals))
 
 }
