2fc7bb35dcc3909baa9f6c51a804018d6dfae73d
Florian Hartig <florianhartig@users.noreply.github.com>
2023-01-27 18:25:34 +0100

    Update phylolm.R

--- Code/DHARMaPackageSupport/phylolm/phylolm.R
+++ Code/DHARMaPackageSupport/phylolm/phylolm.R
@@ -3,6 +3,24 @@
 
 library(phylolm)
 
+# GLM
+
+set.seed(123456)
+tre = rtree(50)
+x = rTrait(n=1,phy=tre)
+X = cbind(rep(1,50),x)
+y = rbinTrait(n=1,phy=tre, beta=c(-1,0.5), alpha=1 ,X=X)
+dat = data.frame(trait01 = y, predictor = x)
+fit = phyloglm(trait01~predictor,phy=tre,data=dat,boot=100, save = F)
+
+summary(fit)
+coef(fit)
+vcov(fit)
+
+DHARMa::simulateResiduals(fit, plot = T)
+
+
+
 # LM 
 
 set.seed(123456)
@@ -23,32 +41,4 @@ z <- y + rnorm(60,0,1)
 dat = data.frame(trait=z[taxa],pred=x[taxa])
 fit = phylolm(trait~pred,data=dat,phy=tre,model="BM",measurement_error=TRUE,boot=100)
 summary(fit)
-
-
-
-
-simulate(fit)
-
-simulateResiduals(fit)
-
-
-# GLM
-
-set.seed(123456)
-tre = rtree(50)
-x = rTrait(n=1,phy=tre)
-X = cbind(rep(1,50),x)
-y = rbinTrait(n=1,phy=tre, beta=c(-1,0.5), alpha=1 ,X=X)
-dat = data.frame(trait01 = y, predictor = x)
-fit = phyloglm(trait01~predictor,phy=tre,data=dat,boot=100, save = F)
-
-summary(fit)
-coef(fit)
-vcov(fit)
-
-fit$bootdata
-fit$bootstrap
-
-predict(fit)
-simulate(fit)
 
